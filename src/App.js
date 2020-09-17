c7bb6ce43bf9efc451b8ca32376aeeaff44036b7
Carles Garcia Bercial <Carles.GarciaBercial@phoenix.edu>
2020-09-17 18:48:33 +0100

    Made component for cube renderer

--- src/App.js
+++ src/App.js
@@ -1,36 +1,13 @@
-import React, { useEffect, useRef } from 'react';
-import * as THREE from 'three';
+import React from 'react';
+import CubeComponent from './components/CubeComponent';
 
 
 function App() {
-  const divRef = useRef(null);
-
-  useEffect(() => {
-    var scene = new THREE.Scene();
-    var camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 1000 );
-    var renderer = new THREE.WebGLRenderer();
-    renderer.setSize( window.innerWidth, window.innerHeight );
-    // document.body.appendChild( renderer.domElement );
-    // use ref as a mount point of the Three.js scene instead of the document.body
-    divRef.current.appendChild( renderer.domElement );
-    var geometry = new THREE.BoxGeometry( 1, 1, 1 );
-    var material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-    var cube = new THREE.Mesh( geometry, material );
-    scene.add( cube );
-    camera.position.z = 5;
-    var animate = function () {
-      requestAnimationFrame( animate );
-      cube.rotation.x += 0.01;
-      cube.rotation.y += 0.01;
-      renderer.render( scene, camera );
-    };
-    animate();
-  }, []);
-
   return (
-    <div className="App" ref={divRef}>
-      
-    </div>
+    <>
+      <CubeComponent />
+      <CubeComponent />
+    </>
   );
 }
 
